bb39e0764028128cd2484ecb8eaa1b540c575995
konradmichalak1 <konradmichalak96@gmail.com>
2018-05-25 13:14:43 +0200

    Zakładka lodówka
    Dodano:
    - Wyszukiwarka wyświetla tylko produkty z bazy danych
    - Lodówka wyświetla produkty zapisane przez użytkownika
    - Dwie listy wyświetlające
    - Nowa ikonka dodawania produktu do lodówki

--- quickmeal/quickmeal/quickmeal/FridgePage.xaml.cs
+++ quickmeal/quickmeal/quickmeal/FridgePage.xaml.cs
@@ -14,17 +14,22 @@ namespace quickmeal
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FridgePage : ContentPage
     {
-        private ListView listView = new ListView();
-        private ObservableCollection<Models.Produkt> products;
+        private ListView listView_produkty = new ListView();
+        private ListView listView_lodowka = new ListView();
+        private ObservableCollection<Models.Lodowka> lodowka;
+        private ObservableCollection<Models.Produkt> produkts;
         public FridgePage()
         {
             InitializeComponent();
-            ViewModels.ProductViewModels viewModels = new ViewModels.ProductViewModels();
-            BindingContext = viewModels;
-            products = viewModels.Products;
+            ViewModels.LodowkaViewModel viewModels_lodowka = new ViewModels.LodowkaViewModel();
+            ViewModels.ProductViewModels viewModels_produkty = new ViewModels.ProductViewModels();
+            BindingContext = viewModels_lodowka;
+            lodowka = viewModels_lodowka.Lodowka;
+            produkts = viewModels_produkty.Products;
+            listView_produkty = this.FindByName<ListView>("ProductList");
+            listView_lodowka = this.FindByName<ListView>("FridgeList");
 
-            listView = this.FindByName<ListView>("FridgeList");
-
+            listView_lodowka.ItemsSource = lodowka;
             SearchBarEventConnect();
 
         }
@@ -35,9 +40,9 @@ namespace quickmeal
             if (x==true)
             {
                 var button = sender as Button;
-                var product = button?.BindingContext as Models.Produkt;
-                var vm = BindingContext as ViewModels.ProductViewModels;
-                vm?.RemoveCommand.Execute(product);
+                var lodowka = button?.BindingContext as Models.Lodowka ;
+                var vm = BindingContext as ViewModels.LodowkaViewModel;
+                vm?.RemoveCommand.Execute(lodowka);
             }
 
         }
@@ -45,12 +50,13 @@ namespace quickmeal
         private void Edit_Clicked(object sender, EventArgs e)
         {
             var button = sender as Button;
-            var product = button?.BindingContext as Models.Produkt;
-            Navigation.PushAsync(new ProductPreview(product));
+            var lodowka = button?.BindingContext as Models.Lodowka;
+            Navigation.PushAsync(new ProductPreview(lodowka)); //Należy zrobić nową zakładke od podglądu lodówki, a ProductPreview będzie do dodawania produktu do lodówki
         }
 
         private void Add_Clicked(object sender, EventArgs e)
         {
+            //Po kliknięciu dodaj, powinna otworzyć się zakładka ProductPreview
             Models.Produkt product = new Models.Produkt();
             var productName = productEntry.Text;
             product.Nazwa = productName;
@@ -67,14 +73,26 @@ namespace quickmeal
         private void SearchBarEventConnect()
         {
             Entry searchBar = this.FindByName<Entry>("productEntry");
+     
             searchBar.TextChanged += (sender, e) =>
             {
-                string keyword = searchBar.Text;
-                IEnumerable<Produkt> searchResult = products.Where(
-                r => r.Nazwa.ToLower().Contains(keyword.ToLower())
-                );
-                listView.ItemsSource = searchResult;
+                if (searchBar.Text != null && searchBar.Text != "")
+                {
+                    listView_produkty.IsVisible = true;
+                    string keyword = searchBar.Text;
+                    IEnumerable<Produkt> searchResult = produkts.Where(
+                    r => r.Nazwa.ToLower().Contains(keyword.ToLower())
+                    );
+                    listView_produkty.ItemsSource = searchResult;
+                }
+                else
+                {
+                    listView_produkty.ItemsSource = null;
+                    listView_produkty.IsVisible = false;
+                }
+
             };
+      
         }
     }
 }
--- quickmeal/quickmeal/quickmeal/Models/Lodowka.cs
+++ quickmeal/quickmeal/quickmeal/Models/Lodowka.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 using SQLite;
+using System.Linq;
 using SQLiteNetExtensions.Attributes;
+
+
 namespace quickmeal.Models
 {
     [Table("Lodowka")]
@@ -14,12 +17,23 @@ namespace quickmeal.Models
         [ForeignKey(typeof(Produkt)), Column("Id_Produktu")]
         public int Id_Produktu { get; set; }
 
+    
+
         [Column("Ilosc")]
         public int Ilosc { get; set; }
 
         public Lodowka(int ilosc)
         {
             Ilosc = ilosc;
+        }
+
+        public string Nazwa { get; set; }
+        public string Gramatura { get; set; }
+        public void Wypelnij_Dane()
+        {
+            Produkt produkt = App.ProduktRepo.GetAllProdukt().Where(x=>x.Id == Id_Produktu).FirstOrDefault();
+            Nazwa = produkt.Nazwa;
+            Gramatura = produkt.Gramatura;
         }
 
         public Lodowka()
